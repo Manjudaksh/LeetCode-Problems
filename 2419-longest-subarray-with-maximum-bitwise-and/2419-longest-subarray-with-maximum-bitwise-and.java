706e9672c13409a09c2fbaa03244606b11a88e94
Manjudaksh <123052350+Manjudaksh@users.noreply.github.com>
2024-09-15 00:38:17 +0530

    Time: 2 ms (100.00%), Space: 60.7 MB (20.79%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
@@ -1,18 +1,21 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int n = nums.length;
-        int max = 0;
+        int n=nums.length;
+        int max=0;
         int c=0,c1=0;
-        for(int i=0; i<n; i++){
+        for(int i=0;i<n;i++)
+        {
             if(max<nums[i])
-                max=nums[i];
+            max=nums[i];
         }
-        for(int i=0; i<n; i++){
-            if(max==nums[i]){
+        for(int i=0;i<n;i++)
+        {
+            if(max==nums[i])
+            {
                 c1++;
                 c=Math.max(c,c1);
             } else
-                c1=0;
+                c1=0;    
         }
         return c;
     }
