0f326443094f58bf41a4121759aa6e39036d660c
Manjudaksh <123052350+Manjudaksh@users.noreply.github.com>
2024-10-14 23:56:35 +0530

    Time: 114 ms (98.58%), Space: 63.1 MB (49.76%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public long maxKelements(int[] nums, int k) {
+//         long score = 0;
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+//         for(int num: nums){
+//             pq.add(num);
+//         }
+//         while(k > 0){
+//             int largest = pq.poll();
+//             score += largest;
+//             int newVal = (int)Math.ceil(largest/3.0);
+//             pq.add(newVal);
+//             k--;
+//         }
+//         return score;
+//     }
+// }
+
 class Solution {
-    public long maxKelements(int[] nums, int k) {
-        long score = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    public long maxKelements(int[] nums, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
         for(int num: nums){
             pq.add(num);
         }
-        while(k > 0){
-            int largest = pq.poll();
-            score += largest;
-            int newVal = (int)Math.ceil(largest/3.0);
-            pq.add(newVal);
-            k--;
+        long output = 0;
+        while(k-- > 0){
+            int n = pq.poll();
+            output += n;
+            pq.offer((n+2)/3);
         }
-        return score;
+        return output;
     }
 }
